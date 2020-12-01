d6d882638c3371c989c540599e1dab5747546276
Nick Yeow <ytknick@gmail.com>
2020-12-02 00:22:59 +0800

    Day 1, part 2

--- day1/solve.py
+++ day1/solve.py
@@ -1,11 +1,14 @@
+import operator
+from functools import reduce
 from itertools import combinations
 
-def solve(nums):
-    for x, y in combinations(nums, 2):
-        if x + y == 2020:
-            return x * y
+def solve(nums, r):
+    for combination in combinations(nums, r):
+        if sum(combination) == 2020:
+            return reduce(operator.mul, combination)
 
 if __name__ == '__main__':
     with open('input.txt') as f:
         nums = [int(line) for line in f]
-        print(solve(nums))
+        print(solve(nums, 2))
+        print(solve(nums, 3))
