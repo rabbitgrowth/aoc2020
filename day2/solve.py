b50e0ceb7123e8e515d73035a6b105a19d98ba2f
Nick Yeow <ytknick@gmail.com>
2020-12-02 13:18:47 +0800

    Day 2, part 2

--- day2/solve.py
+++ day2/solve.py
@@ -3,16 +3,21 @@ import re
 PATTERN = re.compile(r'(\d+)-(\d+) (.): (.+)')
 
 def parse(line):
-    minimum, maximum, letter, password = PATTERN.match(line).groups()
-    minimum = int(minimum)
-    maximum = int(maximum)
-    return minimum, maximum, letter, password
+    x, y, letter, password = PATTERN.match(line).groups()
+    x = int(x)
+    y = int(y)
+    return x, y, letter, password
 
 if __name__ == '__main__':
-    count = 0
+    count1 = 0
+    count2 = 0
     with open('input.txt') as f:
         for line in f:
-            minimum, maximum, letter, password = parse(line)
-            if minimum <= password.count(letter) <= maximum:
-                count += 1
-    print(count)
+            x, y, letter, password = parse(line)
+            if x <= password.count(letter) <= y:
+                count1 += 1
+            if sum([password[x - 1] == letter,
+                    password[y - 1] == letter]) == 1:
+                count2 += 1
+    print(count1)
+    print(count2)
