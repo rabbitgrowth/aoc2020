6920700ed140860766d3e52ee8958bc76956b4b9
Nick Yeow <ytknick@gmail.com>
2020-12-03 14:21:20 +0800

    Rename variables for clarity

--- day3/solve.py
+++ day3/solve.py
@@ -1,25 +1,26 @@
 with open('input.txt') as f:
-    GEOLOGY = f.read().splitlines()
+    MAP = f.read().splitlines()
 
-WIDTH = len(GEOLOGY[0])
+WIDTH = len(MAP[0])
 
-def count_trees(dx, dy):
+def count_trees(slope):
     x, y = 0, 0
+    dx, dy = slope
     trees = 0
     while True:
         x += dx
         y += dy
         try:
-            if GEOLOGY[y][x % WIDTH] == '#':
+            if MAP[y][x % WIDTH] == '#':
                 trees += 1
         except IndexError:
             break
     return trees
 
 if __name__ == '__main__':
-    answer1 = count_trees(3, 1)
+    answer1 = count_trees((3, 1))
     answer2 = 1
-    for dx, dy in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
-        answer2 *= count_trees(dx, dy)
+    for slope in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+        answer2 *= count_trees(slope)
     print(answer1)
     print(answer2)
