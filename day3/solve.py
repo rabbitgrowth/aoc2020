de27821fbffabf6dc737e4a7a58186eac9a935f5
Nick Yeow <ytknick@gmail.com>
2020-12-03 14:19:19 +0800

    Day 3, part 2

--- day3/solve.py
+++ day3/solve.py
@@ -1,17 +1,25 @@
 with open('input.txt') as f:
-    geology = f.read().splitlines()
+    GEOLOGY = f.read().splitlines()
 
-width = len(geology[0])
+WIDTH = len(GEOLOGY[0])
 
-x, y = 0, 0
+def count_trees(dx, dy):
+    x, y = 0, 0
+    trees = 0
+    while True:
+        x += dx
+        y += dy
+        try:
+            if GEOLOGY[y][x % WIDTH] == '#':
+                trees += 1
+        except IndexError:
+            break
+    return trees
 
-trees = 0
-while True:
-    x += 3
-    y += 1
-    try:
-        if geology[y][x % width] == '#':
-            trees += 1
-    except IndexError:
-        break
-print(trees)
+if __name__ == '__main__':
+    answer1 = count_trees(3, 1)
+    answer2 = 1
+    for dx, dy in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+        answer2 *= count_trees(dx, dy)
+    print(answer1)
+    print(answer2)
