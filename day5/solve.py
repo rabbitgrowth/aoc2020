780daf1c240e1a5b60f31ced04166344c731f301
Nick Yeow <ytknick@gmail.com>
2020-12-05 13:58:59 +0800

    Day 5, part 1

---
+++ day5/solve.py
@@ -0,0 +1,23 @@
+def search(chars):
+    lo = 0
+    hi = 2 ** len(chars)
+    diff = hi
+    for char in chars:
+        diff //= 2
+        if char in 'FL':
+            hi -= diff
+        elif char in 'BR':
+            lo += diff
+        else:
+            raise AssertionError(f'Invalid character {char}')
+    assert diff    == 1
+    assert hi - lo == 1
+    return lo
+
+def seat_id(seat):
+    row, column = seat[:7], seat[7:]
+    return search(row) * 8 + search(column)
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        print(max(seat_id(line.strip()) for line in f))
