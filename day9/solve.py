e1ac6e64fd0d7a03a73af0568d7fbf4de0d866f9
Nick Yeow <ytknick@gmail.com>
2020-12-09 14:36:48 +0800

    Day 9, part 2

--- day9/solve.py
+++ day9/solve.py
@@ -4,13 +4,35 @@ from itertools import combinations
 maxlen = 25
 
 with open('input.txt') as f:
-    nums = (int(line) for line in f)
-    queue = deque(maxlen=maxlen)
-    for _ in range(maxlen):
-        queue.append(next(nums))
-    for num in nums:
-        if any(x + y == num for x, y in combinations(queue, 2)):
-            queue.append(num)
-        else:
-            print(num)
+    nums = [int(line) for line in f]
+
+it = iter(nums)
+queue = deque(maxlen=maxlen)
+for _ in range(maxlen):
+    queue.append(next(it))
+for num in it:
+    if any(x + y == num for x, y in combinations(queue, 2)):
+        queue.append(num)
+    else:
+        invalid = num
+        break
+
+print(invalid)
+
+for i, start in enumerate(nums):
+    total, smallest, largest = start, start, start
+    found = False
+    for end in nums[i + 1:]:
+        total += end
+        if end < smallest:
+            smallest = end
+        if end > largest:
+            largest = end
+        if total == invalid:
+            weakness = smallest + largest
+            found = True
             break
+        elif total > invalid:
+            break
+
+print(weakness)
