0d62b7f1d7ab1c6062842eb224b98980b249ba77
Nick Yeow <ytknick@gmail.com>
2020-12-10 16:42:54 +0800

    Smallest item in sorted list is first item

--- day10/solve.py
+++ day10/solve.py
@@ -3,10 +3,8 @@ from collections import Counter
 with open('input.txt') as f:
     nums = sorted(int(line) for line in f)
 
-smallest = min(nums)
-
 # Connections to outlet and built-in adapter
-diffs = Counter([smallest, 3])
+diffs = Counter([nums[0], 3])
 
 for x, y in zip(nums, nums[1:]):
     diffs[y - x] += 1
