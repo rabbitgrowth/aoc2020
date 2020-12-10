d15b9b307d30f25e8618cec583204fa94a23c391
Nick Yeow <ytknick@gmail.com>
2020-12-10 19:21:56 +0800

    Terrible slow recursive solution with global variable
    :-(
    
    Works with the 28, 33, 18, ... dataset and gives the correct 19208,
    but too slow for the real input.

--- day10/solve.py
+++ day10/solve.py
@@ -10,3 +10,19 @@ for x, y in zip(nums, nums[1:]):
     diffs[y - x] += 1
 
 print(diffs[1] * diffs[3])
+
+end = nums[-1]
+numset = set(nums)
+ways = 0
+
+def branch(num):
+    global ways
+    for i in [1, 2, 3]:
+        child = num + i
+        if child == end:
+            ways += 1
+        elif child in numset:
+            branch(child)
+
+branch(0)
+print(ways)
