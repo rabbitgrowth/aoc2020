e9e0abb3287a06f20402063ac4a35ab41ddd82d4
Nick Yeow <ytknick@gmail.com>
2020-12-10 16:39:36 +0800

    Refactor in preparation for part 2

--- day10/solve.py
+++ day10/solve.py
@@ -1,13 +1,14 @@
 from collections import Counter
 
-diffs = Counter()
-
 with open('input.txt') as f:
-    ints = [0]
-    ints.extend(sorted(int(line) for line in f))
-    ints.append(max(ints) + 3)
+    nums = sorted(int(line) for line in f)
 
-for x, y in zip(ints, ints[1:]):
+smallest = min(nums)
+
+# Connections to outlet and built-in adapter
+diffs = Counter([smallest, 3])
+
+for x, y in zip(nums, nums[1:]):
     diffs[y - x] += 1
 
 print(diffs[1] * diffs[3])
