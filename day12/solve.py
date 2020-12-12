348fda37a1b9b5f61b494eb6cde5c654686045a9
Nick Yeow <ytknick@gmail.com>
2020-12-12 15:30:17 +0800

    Refactor in preparation for part 2

--- day12/solve.py
+++ day12/solve.py
@@ -1,9 +1,10 @@
-DIRS = ['E', 'S', 'W', 'N']
+DIRECTIONS = {'E': ( 1,  0),
+              'S': ( 0,  1),
+              'W': (-1,  0),
+              'N': ( 0, -1)}
 
-DXDY = {'E': ( 1,  0),
-        'S': ( 0,  1),
-        'W': (-1,  0),
-        'N': ( 0, -1)}
+def clockwise    (dx, dy): return -dy,  dx
+def anticlockwise(dx, dy): return  dy, -dx
 
 with open('input.txt') as f:
     moves = []
@@ -12,19 +13,23 @@ with open('input.txt') as f:
         num = int(num)
         moves.append((op, num))
 
-x = 0
-y = 0
-index = 0 # direction index
+x, y = 0, 0
+reldx, reldy = DIRECTIONS['E']
 
 for op, num in moves:
-    if op in 'RL':
+    if op in 'LR':
         assert num % 90 == 0
         steps = num // 90
-        index += steps * (1 if op == 'R' else -1)
-    elif op in 'FESWN':
-        dx, dy = DXDY[DIRS[index % 4] if op == 'F' else op]
-        x += dx * num
-        y += dy * num
+        rotate = clockwise if op == 'R' else anticlockwise
+        for _ in range(steps):
+            reldx, reldy = rotate(reldx, reldy)
+    elif op == 'F':
+        x += reldx * num
+        y += reldy * num
+    elif op in 'ESWN':
+        absdx, absdy = DIRECTIONS[op]
+        x += absdx * num
+        y += absdy * num
     else:
         raise ValueError(f'Invalid operation {op}')
 
