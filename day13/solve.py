d78036b8be185878fcc0400d10f45f7b8a4c6fce
Nick Yeow <ytknick@gmail.com>
2020-12-13 15:33:24 +0800

    Parse offsets too

--- day13/solve.py
+++ day13/solve.py
@@ -1,15 +1,20 @@
-from itertools import count
+from collections import namedtuple
+from itertools   import count
+
+Bus = namedtuple('Bus', 'id offset')
 
 with open('input.txt') as f:
     start = int(next(f))
-    buses = [int(bus) for bus in next(f).split(',') if bus != 'x']
+    buses = [Bus(int(bus), offset)
+             for offset, bus in enumerate(next(f).split(','))
+             if bus != 'x']
 
 found = False
 for timestamp in count(start):
     for bus in buses:
-        if timestamp % bus == 0:
+        if timestamp % bus.id == 0:
             wait = timestamp - start
-            print(wait * bus)
+            print(wait * bus.id)
             found = True
             break
     if found:
