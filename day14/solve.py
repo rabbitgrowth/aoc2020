ac06863bf03d04845bb4647ca67bbf7ceae4a1ed
Nick Yeow <ytknick@gmail.com>
2020-12-14 14:53:08 +0800

    Day 14, part 2

--- day14/solve.py
+++ day14/solve.py
@@ -1,4 +1,11 @@
-memory = {}
+from itertools import product
+
+def pad(integer):
+    # 12 (decimal) -> '000000000000000000000000000000001100' (binary)
+    return bin(integer).removeprefix('0b').rjust(36, '0')
+
+memory1 = {}
+memory2 = {}
 
 with open('input.txt') as f:
     for line in f:
@@ -6,9 +13,32 @@ with open('input.txt') as f:
         if lhs == 'mask':
             mask = rhs
         else:
-            index  = int(lhs.removeprefix('mem[').removesuffix(']'))
-            padded = bin(int(rhs)).removeprefix('0b').rjust(36, '0')
-            memory[index] = ''.join(old if new == 'X' else new
-                                    for old, new in zip(padded, mask))
+            # mem[index] = value
+            #     ^^^^^    ^^^^^ ints
+            index = int(lhs.removeprefix('mem[').removesuffix(']'))
+            value = int(rhs)
+            # Part 1
+            masked = ''.join(old if new == 'X' else new
+                             for old, new in zip(pad(value), mask))
+            memory1[index] = int(masked, base=2)
+            # Part 2
+            floating  = []
+            x_indices = []
+            for index, (old, new) in enumerate(zip(pad(index), mask)):
+                if new == '0':
+                    floating.append(old)
+                elif new == '1':
+                    floating.append('1')
+                elif new == 'X':
+                    floating.append(None) # filler
+                    x_indices.append(index)
+                else:
+                    raise ValueError(f'Invalid character {new}')
+            for choices in product('01', repeat=len(x_indices)):
+                for x_index, choice in zip(x_indices, choices):
+                    floating[x_index] = choice
+                index = int(''.join(floating), base=2) # convert to decimal just to be neat
+                memory2[index] = value
 
-print(sum(int(value, base=2) for value in memory.values()))
+print(sum(memory1.values()))
+print(sum(memory2.values()))
