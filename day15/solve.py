d16cab47aa7b6e2c834d16c58677a58df5f5a3d1
Nick Yeow <ytknick@gmail.com>
2020-12-15 17:16:32 +0800

    Make indices start at 1 and therefore less confusing

--- day15/solve.py
+++ day15/solve.py
@@ -1,9 +1,11 @@
+from itertools import count
+
 INPUT = [12, 20, 0, 6, 1, 17, 7]
 
-haystack = {number: index for index, number in enumerate(INPUT[:-1])}
+haystack = dict(zip(INPUT[:-1], count(1)))
 needle   = INPUT[-1]
 
-turn = len(haystack)
+turn = len(INPUT)
 
 while True:
     try:
@@ -14,9 +16,9 @@ while True:
         number = turn - index
     haystack[needle] = turn
     turn += 1
-    if turn == 2020 - 1: # I know it's inefficient to check this ~30,000,000 times
+    if turn == 2020: # I know it's inefficient to check this ~30,000,000 times
         print(number)
-    elif turn == 30_000_000 - 1:
+    elif turn == 30_000_000:
         print(number)
         break
     needle = number
