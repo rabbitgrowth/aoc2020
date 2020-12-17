629eec1c57c192d21c10f465e5379b4b098547f0
Nick Yeow <ytknick@gmail.com>
2020-12-17 13:15:56 +0800

    Clean up

--- day16/solve.py
+++ day16/solve.py
@@ -1,6 +1,9 @@
 import re
 from itertools import chain
 
+def ints(line):
+    return tuple(map(int, line.split(',')))
+
 with open('input.txt') as f:
     par1, par2, par3 = f.read().split('\n\n')
     constraints = {}
@@ -8,9 +11,8 @@ with open('input.txt') as f:
         field, rhs = line.split(': ')
         min1, max1, min2, max2 = map(int, re.findall(r'\d+', rhs))
         constraints[field] = (range(min1, max1 + 1), range(min2, max2 + 1))
-    mine = list(map(int, par2.splitlines()[1].split(',')))
-    nearby = [list(map(int, line.split(',')))
-              for line in par3.splitlines()[1:]]
+    mine = ints(par2.splitlines()[1])
+    nearby = [ints(line) for line in par3.splitlines()[1:]]
 
 valid_nearby = []
 error_rate = 0
@@ -29,24 +31,21 @@ for values in nearby:
 print(error_rate) # Part 1
 
 rows    = chain((mine,), valid_nearby)
-columns = [list(values) for values in zip(*rows)]
+columns = list(zip(*rows))
 
-choices = [(index, {field
-                    for field, ranges in constraints.items()
-                    if all(any(value in range_ for range_ in ranges)
-                           for value in column)})
-           for index, column in enumerate(columns)]
+choices = [{field
+            for field, ranges in constraints.items()
+            if all(any(value in range_ for range_ in ranges)
+                   for value in column)}
+           for column in columns]
 
 seen              = set()
 translated_fields = []
 
-for index, fields in sorted(choices, key=lambda tpl: len(tpl[1])):
+answer = 1
+for index, fields in sorted(enumerate(choices), key=lambda tpl: len(tpl[1])):
     (field,) = fields - seen # only works for this particular input
     seen.add(field)
-    translated_fields.append((index, field))
-
-answer = 1
-for index, field in translated_fields:
     if field.startswith('departure'):
         answer *= mine[index]
 print(answer) # Part 2
