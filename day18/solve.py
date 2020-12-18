2c0eb8ca364af485a500933ff7e20cc66bca4f5c
Nick Yeow <ytknick@gmail.com>
2020-12-18 17:55:54 +0800

    Support parentheses with stack
    Requires manually adding outer ( ).
    
    Naming:
    group()    -> evaluate()
    evaluate() -> flat_evaluate() ?
    
    Is it possible to merge the two functions into one evaluate() ???
    
    This implementation feels very buggy. I think it would fail miserably if
    given input that's not strictly syntactically correct.

--- day18/solve.py
+++ day18/solve.py
@@ -1,21 +1,38 @@
-from itertools import chain
-from operator  import add, mul
+import re
+from operator import add, mul
 
+WORD = re.compile(r'\d+|[+*()]')
 FUNCTIONS = {'+': add, '*': mul}
 
 def tokenize(expression):
-    for word in expression.split():
-        yield FUNCTIONS[word] if word in FUNCTIONS else int(word)
+    for word in WORD.findall(expression):
+        if word in FUNCTIONS:
+            yield FUNCTIONS[word]
+        elif word in '()':
+            yield word
+        else:
+            yield int(word)
+
+def group(tokens):
+    groups = []
+    for token in tokens:
+        if token == '(':
+            groups.append([])
+        elif token == ')':
+            result = evaluate(groups.pop())
+            if not groups:
+                return result
+            groups[-1].append(result)
+        else:
+            groups[-1].append(token)
 
 def evaluate(tokens):
-    x = next(tokens)
-    try:
-        function, y = next(tokens), next(tokens)
-    except StopIteration:
-        return x
+    if len(tokens) == 1:
+        return tokens[0]
+    x, function, y = tokens[:3]
     result = function(x, y)
-    return evaluate(chain((result,), tokens))
+    return evaluate([result, *tokens[3:]])
 
 def calculate(expression):
     tokens = tokenize(expression)
-    return evaluate(tokens)
+    return group(tokens)
