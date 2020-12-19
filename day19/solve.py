89ae1615f143c8c82bb62a4ebd4af5fe55e91b6e
Nick Yeow <ytknick@gmail.com>
2020-12-19 22:26:35 +0800

    Day 19, part 1

--- day19/solve.py
+++ day19/solve.py
@@ -1,31 +1,39 @@
-# 0: 4 1 5
-# 1: 2 3 | 3 2
-# 2: 4 4 | 5 5
-# 3: 4 5 | 5 4
-# 4: "a"
-# 5: "b"
+import re
 
-rules = {
-    0: (4, 1, 5),
-    1: [(2, 3), (3, 2)],
-    2: [(4, 4), (5, 5)],
-    3: [(4, 5), (5, 4)],
-    4: ('a',),
-    5: ('b',),
-}
+def parse(paragraph):
+    grammar = {}
+    for line in paragraph.splitlines():
+        lhs, rhs = line.split(': ')
+        rule = int(lhs)
+        if '|' in rhs:   # e.g., 2 3 | 3 2
+            expansion = [tuple(map(int, choice.split()))
+                         for choice in rhs.split(' | ')]
+        elif '"' in rhs: # e.g., "a"
+            expansion = tuple(rhs.removeprefix('"').removesuffix('"'))
+        else:            # e.g., 4 1 5
+            expansion = tuple(map(int, rhs.split()))
+        grammar[rule] = expansion
+    return grammar
 
-def expand(tokens):
+def expand(tokens, grammar):
     result = ''
     for token in tokens:
-        if token not in rules: # cannot be expanded further
+        if token not in grammar: # cannot be expanded further
             result += token
         else:
-            expansion = rules.get(token)
+            expansion = grammar.get(token)
             if isinstance(expansion, list):
-                choices = '|'.join(map(expand, expansion))
-                result += f'({choices})'
+                choices = '|'.join(expand(choice, grammar)
+                                   for choice in expansion)
+                result += f'(?:{choices})'
             else:
-                result += expand(expansion)
+                result += expand(expansion, grammar)
     return result
 
-print(expand((0,)))
+with open('input.txt') as f:
+    par1, par2 = f.read().split('\n\n')
+    grammar  = parse(par1)
+    regex    = re.compile('^' + expand((0,), grammar) + '$')
+    messages = par2.splitlines()
+    print(sum(regex.search(message) is not None
+              for message in messages))
