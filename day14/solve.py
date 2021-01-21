291d90be26eaf2ca1ca9213890e05406fcb51a54
Nick Yeow <ytknick@gmail.com>
2021-01-21 20:46:58 +0800

    Day 14: use itertools.product more smartly

--- day14/solve.py
+++ day14/solve.py
@@ -18,26 +18,20 @@ with open('input.txt') as f:
             index = int(lhs.removeprefix('mem[').removesuffix(']'))
             value = int(rhs)
             # Part 1
-            masked = ''.join(old if new == 'X' else new
-                             for old, new in zip(pad(value), mask))
+            masked = ''.join(v if m == 'X' else m
+                             for v, m in zip(pad(value), mask))
             memory1[index] = int(masked, base=2)
             # Part 2
-            floating  = []
-            x_indices = []
-            for index, (old, new) in enumerate(zip(pad(index), mask)):
-                if new == '0':
-                    floating.append(old)
-                elif new == '1':
-                    floating.append('1')
-                elif new == 'X':
-                    floating.append(None) # filler
-                    x_indices.append(index)
-                else:
-                    raise ValueError(f'Invalid character {new}')
-            for choices in product('01', repeat=len(x_indices)):
-                for x_index, choice in zip(x_indices, choices):
-                    floating[x_index] = choice
-                index = int(''.join(floating), base=2) # convert to decimal just to be neat
+            masked = []
+            for i, m in zip(pad(index), mask):
+                if m == '0':
+                    masked.append(i)
+                elif m == '1':
+                    masked.append('1')
+                elif m == 'X':
+                    masked.append('01')
+            for combination in product(*masked):
+                index = int(''.join(combination), base=2) # convert to decimal just to be neat
                 memory2[index] = value
 
 print(sum(memory1.values()))
