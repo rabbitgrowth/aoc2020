218746a8083e94bf90c823d0322c65c733fb7cff
Nick Yeow <ytknick@gmail.com>
2021-01-23 15:33:33 +0800

    Use .sum instead of .reduce(:+)

--- day1/solve.rb
+++ day1/solve.rb
@@ -1,6 +1,6 @@
 File.open('input.txt') do |file|
   nums = file.readlines.map(&:to_i)
   [2, 3].each do |r|
-    puts nums.combination(r).find { |nums| nums.reduce(:+) == 2020 }.reduce(:*)
+    puts nums.combination(r).find { |nums| nums.sum == 2020 }.reduce(:*)
   end
 end
