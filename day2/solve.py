9aa83ba58b9a78189bf80408acd1b954a8489971
Nick Yeow <ytknick@gmail.com>
2021-01-23 17:18:26 +0800

    Clean up Day 2

--- day2/solve.py
+++ day2/solve.py
@@ -8,15 +8,16 @@ def parse(line):
     y = int(y)
     return x, y, letter, password
 
-if __name__ == '__main__':
-    count1 = 0
-    count2 = 0
-    with open('input.txt') as f:
-        for line in f:
-            x, y, letter, password = parse(line)
-            if x <= password.count(letter) <= y:
-                count1 += 1
-            if sum(password[index - 1] == letter for index in [x, y]) == 1:
-                count2 += 1
-    print(count1)
-    print(count2)
+count1 = 0
+count2 = 0
+
+with open('input.txt') as f:
+    for line in f:
+        x, y, letter, password = parse(line)
+        if x <= password.count(letter) <= y:
+            count1 += 1
+        if sum(password[index - 1] == letter for index in [x, y]) == 1:
+            count2 += 1
+
+print(count1)
+print(count2)
