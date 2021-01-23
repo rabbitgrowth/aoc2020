003d3a5960516a163e844527da10583c4e730124
Nick Yeow <ytknick@gmail.com>
2021-01-23 17:17:42 +0800

    Clean up Day 1

--- day1/solve.py
+++ day1/solve.py
@@ -1,13 +1,10 @@
 import math
 from itertools import combinations
 
-def solve(nums, r):
+with open('input.txt') as f:
+    nums = [int(line) for line in f]
+
+for r in (2, 3):
     for combination in combinations(nums, r):
         if sum(combination) == 2020:
-            return math.prod(combination)
-
-if __name__ == '__main__':
-    with open('input.txt') as f:
-        nums = [int(line) for line in f]
-    print(solve(nums, 2))
-    print(solve(nums, 3))
+            print(math.prod(combination))
