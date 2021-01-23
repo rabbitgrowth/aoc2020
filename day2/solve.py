a83ab050465b6cfeaec2cb6c55e4c44d6c3756fd
Nick Yeow <ytknick@gmail.com>
2021-01-23 17:33:28 +0800

    Further simplify Day 2

--- day2/solve.py
+++ day2/solve.py
@@ -2,18 +2,14 @@ import re
 
 PATTERN = re.compile(r'(\d+)-(\d+) (.): (.+)')
 
-def parse(line):
-    x, y, letter, password = PATTERN.match(line).groups()
-    x = int(x)
-    y = int(y)
-    return x, y, letter, password
-
 count1 = 0
 count2 = 0
 
 with open('input.txt') as f:
     for line in f:
-        x, y, letter, password = parse(line)
+        x, y, letter, password = PATTERN.match(line).groups()
+        x = int(x)
+        y = int(y)
         if x <= password.count(letter) <= y:
             count1 += 1
         if sum(password[index - 1] == letter for index in [x, y]) == 1:
