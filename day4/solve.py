7805cc478ccac0e839daaf11cadc0e3064402be1
Nick Yeow <ytknick@gmail.com>
2021-01-26 19:03:53 +0800

    Clean up Day 4

--- day4/solve.py
+++ day4/solve.py
@@ -1,35 +1,34 @@
 import re
 
-def verify_hgt(hgt):
+ECLS = {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'}
+
+def validate_hgt(hgt):
     match = re.search(r'^(\d+)(cm|in)$', hgt)
     if not match:
         return False
+    num, unit = match.groups()
+    num = int(num)
+    if unit == 'cm':
+        return 150 <= num <= 193
     else:
-        num, unit = match.groups()
-        num = int(num)
-        if unit == 'cm':
-            return 150 <= num <= 193
-        elif unit == 'in':
-            return 59 <= num <= 76
-        else:
-            raise AssertionError("This shouldn't happen")
+        return 59 <= num <= 76
 
-fields = {
+FIELDS = {
     'byr': lambda byr: 1920 <= int(byr) <= 2002,
     'iyr': lambda iyr: 2010 <= int(iyr) <= 2020,
     'eyr': lambda eyr: 2020 <= int(eyr) <= 2030,
-    'hgt': verify_hgt,
+    'hgt': validate_hgt,
     'hcl': lambda hcl: re.search(r'^#[0-9a-f]{6}$', hcl) is not None,
-    'ecl': lambda ecl: ecl in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'},
+    'ecl': lambda ecl: ecl in ECLS,
     'pid': lambda pid: re.search(r'^[0-9]{9}$', pid) is not None,
     # cid is ignored
 }
 
 def validate1(passport):
-    return all(field in passport for field in fields)
+    return all(field in passport for field in FIELDS)
 
 def validate2(passport):
-    return all(fields.get(key, lambda value: True)(value)
+    return all(FIELDS.get(key, lambda value: True)(value)
                for key, value in passport.items())
 
 with open('input.txt') as f:
