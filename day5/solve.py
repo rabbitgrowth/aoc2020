e2794e5417b19d7ea9e32673e715a2dc22dc5011
Nick Yeow <ytknick@gmail.com>
2021-01-26 21:38:55 +0800

    Seat ID is just binary number in disguise

--- day5/solve.py
+++ day5/solve.py
@@ -1,28 +1,13 @@
-def search(chars):
-    lo = 0
-    hi = 2 ** len(chars)
-    diff = hi
-    for char in chars:
-        diff //= 2
-        if char in 'FL':
-            hi -= diff
-        elif char in 'BR':
-            lo += diff
-        else:
-            raise AssertionError(f'Invalid character {char}')
-    assert diff    == 1
-    assert hi - lo == 1
-    return lo
+TABLE = str.maketrans('FBLR', '0101')
 
 def seat_id(seat):
-    row, column = seat[:7], seat[7:]
-    return search(row) * 8 + search(column)
+    return int(seat.translate(TABLE), 2)
 
-if __name__ == '__main__':
-    with open('input.txt') as f:
-        seat_ids = {seat_id(line.strip()) for line in f}
-        print(max(seat_ids))
-        for seat_id in sorted(seat_ids):
-            if seat_id + 1 not in seat_ids:
-                print(seat_id + 1)
-                break
+with open('input.txt') as f:
+    seat_ids = set(map(seat_id, f))
+
+print(max(seat_ids))
+for seat_id in seat_ids:
+    if seat_id + 1 not in seat_ids:
+        print(seat_id + 1)
+        break
