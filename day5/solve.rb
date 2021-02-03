e75b47b695e4d447cf2538ddaf054274f3bc1b07
Nick Yeow <ytknick@gmail.com>
2021-02-03 15:52:31 +0800

    Only tr once

--- day5/solve.rb
+++ day5/solve.rb
@@ -1,6 +1,4 @@
-seats = File.foreach('input.txt').map do |seat|
-  seat.tr('FBLR', '0101').to_i(2)
-end
+seats = File.read('input.txt').tr('FBLR', '0101').lines.map { |line| line.to_i(2) }
 
 puts seats.max
 puts seats.sort.each_cons(2).find { |a, b| b - a == 2 }.first + 1
