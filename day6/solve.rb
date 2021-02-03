724217d4ca90c06ef0dff2b12a038b71b06c70c2
Nick Yeow <ytknick@gmail.com>
2021-02-03 16:30:33 +0800

    Use | and & symbols

--- day6/solve.rb
+++ day6/solve.rb
@@ -1,4 +1,4 @@
 groups = File.read('input.txt').split("\n\n").map { |group| group.split.map(&:chars) }
 
-puts groups.map { |group| group.reduce(:union).size }.sum
-puts groups.map { |group| group.reduce(:intersection).size }.sum
+puts groups.map { |group| group.reduce(:|).size }.sum
+puts groups.map { |group| group.reduce(:&).size }.sum
