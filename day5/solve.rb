671948c318abd811692a2d6426ec05678afbe8a4
Nick Yeow <ytknick@gmail.com>
2021-02-03 15:55:34 +0800

    Use set difference to find seat

--- day5/solve.rb
+++ day5/solve.rb
@@ -1,4 +1,4 @@
 seats = File.read('input.txt').tr('FBLR', '0101').lines.map { |line| line.to_i(2) }
 
 puts seats.max
-puts seats.sort.each_cons(2).find { |a, b| b - a == 2 }.first + 1
+puts (seats.min..seats.max).to_a - seats
