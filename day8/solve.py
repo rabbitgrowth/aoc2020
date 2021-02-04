bc6bff4408231d147745c7b82247bce20053d205
Nick Yeow <ytknick@gmail.com>
2021-02-04 16:15:45 +0800

    Clean up Day 8

--- day8/solve.py
+++ day8/solve.py
@@ -1,18 +1,17 @@
 from copy import copy
 
-def parse(file):
-    program = []
-    with open(file) as f:
-        for line in f:
-            operation, argument = line.split()
-            argument = int(argument)
-            program.append((operation, argument))
-    return program
+program = []
+
+with open('input.txt') as f:
+    for line in f:
+        operation, argument = line.split()
+        program.append((operation, int(argument)))
 
 def run(program):
     pointer     = 0
     accumulator = 0
-    seen = set()
+    seen        = set()
+
     while True:
         seen.add(pointer)
         operation, argument = program[pointer]
@@ -28,23 +27,21 @@ def run(program):
         if pointer == len(program):
             return accumulator, 0 # "program terminated normally"
 
-if __name__ == '__main__':
-    program = parse('input.txt')
+# Part 1
+accumulator, exit_status = run(program)
+assert exit_status == 1
+print(accumulator)
 
-    # Part 1
-    accumulator, exit_status = run(program)
-    assert exit_status == 1
-    print(accumulator)
-
-    # Part 2
-    for i, (operation, argument) in enumerate(program):
-        program_copy = copy(program)
-        if operation == 'jmp':
-            program_copy[i] = ('nop', argument)
-        elif operation == 'nop':
-            program_copy[i] = ('jmp', argument)
-        else:
-            continue
-        accumulator, exit_status = run(program_copy)
-        if exit_status == 0:
-            print(accumulator)
+# Part 2
+for i, (operation, argument) in enumerate(program):
+    program_copy = copy(program)
+    if operation == 'jmp':
+        program_copy[i] = ('nop', argument)
+    elif operation == 'nop':
+        program_copy[i] = ('jmp', argument)
+    else:
+        continue
+    accumulator, exit_status = run(program_copy)
+    if exit_status == 0:
+        print(accumulator)
+        break
